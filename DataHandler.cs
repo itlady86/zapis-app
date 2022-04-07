c517ffee9a6297b2244fade5ededc93bdc4adeb4
Lucie Názrová <nazrova.lucie@centrum.cz>
2022-04-07 13:05:50 +0200

    Třída DataHandler, naplnění combobox1 z listu ve třídě DH

---
+++ DataHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zapis_app
+{
+    public class DataHandler
+    {
+        public List<string> zakladniSkoly = new List<string> { "Čapkova", "Masarykova", "Plánická", "Tolstého" };
+        public string rok = "2022/2023";
+        public string akt_rok = "";
+
+
+        
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -2,15 +2,45 @@ namespace zapis_app
 {
     public partial class Form1 : Form
     {
+        public DataHandler dh = new DataHandler();
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillCombobox(); 
+            
+        }
+        
+
+
+
+        // metoda pro naplnìní položek comboboxu
+        private void FillCombobox()
+        {
+            foreach(var zs in dh.zakladniSkoly)
+            {
+                comboBox1.Items.Add(zs);
+                comboBox1.SelectedIndex = 0;
+                //System.Diagnostics.Debug.WriteLine(zs);
+
+
+            } 
+            
+        }
+
+
+
 
         // tlaèítko Pokraèovat
         private void button1_Click(object sender, EventArgs e)
         {
 
         }
+
     }
 }
